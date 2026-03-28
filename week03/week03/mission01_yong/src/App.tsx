345e60eb34b3b043a3588f0f3aa4a0ec70fdd646
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-03-28 15:53:31 +0900

    mission1 초기 세팅, 용코딩 영상

---
+++ week03/week03/mission01_yong/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_TMDB_KEY);
+  return (
+    <>
+      <h1 className="text-2x text-amber-600">HI</h1>
+    </>
+  );
+}
+
+export default App;
