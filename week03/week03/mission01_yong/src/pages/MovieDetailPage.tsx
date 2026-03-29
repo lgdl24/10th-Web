89944b0475947b6fe18110d6faa7e7af0e227e4d
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-03-29 14:46:27 +0900

    App: route변경, Navbar: Link 변경

--- week03/week03/mission01_yong/src/pages/MovieDetailPage.tsx
+++ week03/week03/mission01_yong/src/pages/MovieDetailPage.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import type { MovieDetails } from "../types/movie";
 import axios from "axios";
 
 export default function MovieDetailPage() {
   const [movieDetails, setMovieDetails] = useState<MovieDetails>();
-  const params = useParams();
+  const { movie_id } = useParams();
   //https://api.themoviedb.org/3/movie/{movie_id}
-
+  console.log("렌더됨");
   useEffect(() => {
     const getMovieDetails = async (): Promise<void> => {
-      const data = await axios.get(
-        `https://api.themoviedb.org/3/movie/{movie_id}`,
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movie_id}`,
         {
           headers: {
             Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -20,8 +20,14 @@ export default function MovieDetailPage() {
       );
       //console.log(data.data);
       setMovieDetails(data);
-      console.log(movieDetails);
+      console.log(data);
     };
-  });
-  return <div></div>;
+    console.log("이규동");
+    getMovieDetails();
+  }, [movie_id]);
+  return (
+    <div>
+      <h1>{movieDetails?.title}</h1>
+    </div>
+  );
 }
