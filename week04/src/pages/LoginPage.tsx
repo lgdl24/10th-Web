d56c280fa9a0bd9bd3e7f1aae22d55f5c8035161
lgdl24 <rkfnal77@skuniv.ac.kr>
2026-04-07 19:28:10 +0900

    feat : week04/mission02 완성

---
+++ week04/src/pages/LoginPage.tsx
@@ -0,0 +1,65 @@
+import useForm from "../hooks/useForm";
+import { validateSignin, type UserSigininInformation } from "../utils/validate";
+
+const LoginPage = () => {
+  const { values, error, touched, getInputProps } =
+    useForm<UserSigininInformation>({
+      initailValue: {
+        email: "",
+        password: "",
+      },
+      validate: validateSignin,
+    });
+
+  const handleSubmit = () => {
+    console.log(values);
+  };
+
+  const isDisabled =
+    Object.values(error || {}).some((error: string) => error.length > 0) ||
+    Object.values(values).some((value) => value === "");
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-4">
+      <div className="flex flex-col gap-3">
+        <input
+          {...getInputProps("email")}
+          name="email"
+          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
+            error.email && touched.email
+              ? "border-red-500 bg-red-200"
+              : "border-gray-300"
+          }`}
+          type={"email"}
+          placeholder={"이메일"}
+        />
+        {error.email && touched.email && (
+          <div className="text-red-500 text-sm">{error.email}</div>
+        )}
+        <input
+          {...getInputProps("password")}
+          className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
+            error.password && touched.password
+              ? "border-red-500 bg-red-200"
+              : "border-gray-300"
+          }`}
+          type={"password"}
+          placeholder={"비밀번호"}
+        />
+        {error.password && touched.password && (
+          <div className="text-red-500 text-sm">{error.password}</div>
+        )}
+        <button
+          type="button"
+          onClick={handleSubmit}
+          disabled={isDisabled}
+          className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-400"
+        >
+          로그인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
